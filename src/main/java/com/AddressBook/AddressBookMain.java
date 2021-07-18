4dd6038b4b7a171dae092585e5a573cf6753df66
balu-pinisety <manikantapandu225@gmail.com>
2021-07-18 23:00:38 +0530

    Added: File for Address Book Program

---
+++ src/main/java/com/AddressBook/AddressBookMain.java
@@ -0,0 +1,14 @@
+package com.AddressBook;
+
+/**
+ * @author BALU
+ * Main Class for AddressBook
+ */
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+		// Displaying Welcome Message
+		System.out.println("Welcome to Address Book Program");
+	}
+
+}
